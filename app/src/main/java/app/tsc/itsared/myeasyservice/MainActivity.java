c2c96b32e08338d5ad1052243ecee88a53776cd7
itsared.h <littlefatboyz@gmail.com>
2017-10-21 15:59:09 +0700

    finish 21oct17

--- app/src/main/java/app/tsc/itsared/myeasyservice/MainActivity.java
+++ app/src/main/java/app/tsc/itsared/myeasyservice/MainActivity.java
@@ -3,11 +3,23 @@ package app.tsc.itsared.myeasyservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import app.tsc.itsared.myeasyservice.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragmentMain,new MainFragment()).commit();
+        }
+
+
+
+    } //Main Method
+
+
+} //Main Class
