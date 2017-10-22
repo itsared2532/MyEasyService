778c78d7e85cb17b741e8f6a4d8ba6391ee3cd11
itsared.h <littlefatboyz@gmail.com>
2017-10-22 13:50:46 +0700

    Drawer Close OK

--- app/src/main/java/app/tsc/itsared/myeasyservice/MainActivity.java
+++ app/src/main/java/app/tsc/itsared/myeasyservice/MainActivity.java
@@ -1,25 +1,84 @@
 package app.tsc.itsared.myeasyservice;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import app.tsc.itsared.myeasyservice.fragment.MainFragment;
 
+import static app.tsc.itsared.myeasyservice.R.id.myDrawerLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.contentFragmentMain,new MainFragment()).commit();
-        }
+        // Add Fragment to Activity
+        addFragment(savedInstanceState);
+
+        // Setup Drawer Menu
+        setupDrawerMenu();
+
+
+        // Assign ; -> ctrl + Shift + enter
+        // Extract Method -> ctrl + alt + M
+
+        // Text Controller
+        textController();
+
+
 
 
 
     } //Main Method
+
+    private void textController() {
+        TextView mainTextView = (TextView) findViewById(R.id.txtMainFragment);
+        TextView secondTextView = (TextView) findViewById(R.id.txtSecondFragment);
+        TextView exitTextView = (TextView) findViewById(R.id.txtExit);
+
+        //For Main Fragment
+        mainTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Close Drawer
+                drawerLayout.closeDrawers();
+            }
+        });
+        //For Second Fragment
+        secondTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Close Drawer
+                drawerLayout.closeDrawers();
+            }
+        });
+        //For Exit
+        exitTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Close Drawer
+                drawerLayout.closeDrawers();
+            }
+        });
+    }
+
+    private void setupDrawerMenu() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.myDrawerLayout);
+    }
+
+    private void addFragment(Bundle savedInstanceState) {
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragmentMain,new MainFragment()).commit();
+        }
+    }
 
 
 } //Main Class
