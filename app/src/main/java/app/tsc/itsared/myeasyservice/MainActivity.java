cf00c3a125f1552337cadd5b2f91c6d100ee7d5e
itsared.h <littlefatboyz@gmail.com>
2017-10-22 14:44:18 +0700

    Hambeger Icon OK

--- app/src/main/java/app/tsc/itsared/myeasyservice/MainActivity.java
+++ app/src/main/java/app/tsc/itsared/myeasyservice/MainActivity.java
@@ -1,18 +1,26 @@
 package app.tsc.itsared.myeasyservice;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import app.tsc.itsared.myeasyservice.fragment.MainFragment;
+import app.tsc.itsared.myeasyservice.fragment.SecondFragment;
 
 import static app.tsc.itsared.myeasyservice.R.id.myDrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +33,57 @@ public class MainActivity extends AppCompatActivity {
         // Setup Drawer Menu
         setupDrawerMenu();
 
-
         // Assign ; -> ctrl + Shift + enter
         // Extract Method -> ctrl + alt + M
 
         // Text Controller
         textController();
 
-
-
-
+        // Create Toolbar
+        createToolbar();
 
     } //Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        actionBarDrawerToggle.syncState();
+
+    }
+
+    private void createToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolBarMain);
+        setSupportActionBar(toolbar);
+
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                MainActivity.this,
+                drawerLayout,
+                R.string.open,
+                R.string.close
+        );
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
 
     private void textController() {
         TextView mainTextView = (TextView) findViewById(R.id.txtMainFragment);
@@ -47,6 +94,10 @@ public class MainActivity extends AppCompatActivity {
         mainTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentFragmentMain, new MainFragment()).commit();
+
                 //Close Drawer
                 drawerLayout.closeDrawers();
             }
@@ -55,6 +106,11 @@ public class MainActivity extends AppCompatActivity {
         secondTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentFragmentMain, new SecondFragment()).commit();
+
                 //Close Drawer
                 drawerLayout.closeDrawers();
             }
@@ -63,8 +119,11 @@ public class MainActivity extends AppCompatActivity {
         exitTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                finish();
+
                 //Close Drawer
-                drawerLayout.closeDrawers();
+                //drawerLayout.closeDrawers();
+
             }
         });
     }
@@ -76,7 +135,7 @@ public class MainActivity extends AppCompatActivity {
     private void addFragment(Bundle savedInstanceState) {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.contentFragmentMain,new MainFragment()).commit();
+                    .add(R.id.contentFragmentMain, new MainFragment()).commit();
         }
     }
 
